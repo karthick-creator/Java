675733d0ab0311e3f153accf8afa2d578758bc03
karthik <karthickdec19@gmail.com>
2022-09-10 14:44:27 +0530

    Second commit

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+class Calculator
+{
+public static void main(String[] args)
+{
+Calculator calc = new Calculator();
+calc.add();
+
+}
+void add()
+{
+int no1 = 5, no2 = 10, no3 = 25;
+int result = no1 + no2 + no3;
+System.out.println(result);
+
+}
+}
